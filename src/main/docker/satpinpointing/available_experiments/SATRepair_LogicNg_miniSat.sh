7fc181ebf094d13e711ab0c2cd8bccfc9f8a26c3
joergschwabe <joerg.schwabe@uni-ulm.de>
2019-08-29 10:26:30 +0200

    add logic nc solver

---
+++ src/main/docker/satpinpointing/available_experiments/SATRepair_LogicNg_miniSat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+TIMEOUT=$1
+shift
+GLOBAL_TIMEOUT=$1
+shift
+QUERY_FILE=$1
+shift
+ENCODING_DIR=$1
+shift
+SCRIPTS_DIR=$1
+shift
+OUTPUT_DIR=$1
+shift
+
+java $JAVA_MEMORY_OPTIONS -Dlog4j.configurationFile=log4j2-paramfiles.xml -Dlog.file.out=$OUTPUT_DIR/out.log -Dlog.file.err=$OUTPUT_DIR/err.log -cp "$CLASSPATH" com.github.joergschwabe.RunJustificationExperiments -t "$TIMEOUT"000 -g "$GLOBAL_TIMEOUT"000 --progress $OUTPUT_DIR/record.csv $QUERY_FILE com.github.joergschwabe.experiments.SatFactoryJustificationExperiment -- $ENCODING_DIR com.github.joergschwabe.SatRepairComputationLogicNg_miniSat
